78e40f262afc05db868e5b59e2549ade50af1801
Thalles Bruno <thallesbgns@gmail.com>
2021-07-30 22:57:25 -0300

    Commit inicial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            int result = n1 / n2;
+            Console.WriteLine(result);
+        }
+    }
+}
